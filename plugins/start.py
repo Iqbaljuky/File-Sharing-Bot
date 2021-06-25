3dd44c50df8ce47c825e40fe198a2ce52d547a32
hardolando <86427308+hardolando@users.noreply.github.com>
2021-06-25 21:26:09 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -94,14 +94,14 @@ async def start_command(client: Client, message: Message):
 
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
-    text = "<b>Join Dulu lah sayang. Lu kira gampang bikin ginian? Susah gblk, Gada yang gratis. Subs buru biar kita sama sama untung\n\nGak Join Mandul</b>"
+    text = "<b>Join Dulu lah sayang\nLu kira gampang bikin ginian\nSusah gblk\n\nGada yang gratis\nSubs buru biar kita sama sama untung\n\nGak Join Mandul</b>"
     message_text = message.text
     try:
         command, argument = message_text.split()
-        text = text + f" <b>and <a href='https://t.me/{client.username}?start={argument}'>coba lagi</a></b>"
+        text = text + f" <b>and <a href='https://t.me/{client.username}?start={argument}'>try again</a></b>"
     except ValueError:
         pass
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("BURU JOIN!", url = client.invitelink)]])
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("BURU JOIN", url = client.invitelink)]])
     await message.reply(
         text = text,
         reply_markup = reply_markup,
