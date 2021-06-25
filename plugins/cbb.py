8c480c17a63674bf4c90b551036270031c0c6c84
hardolando <86427308+hardolando@users.noreply.github.com>
2021-06-25 16:01:22 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,12 +10,12 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>",
+            text = f"<b>○ MY LORD : <a href='tg://user?id={OWNER_ID}'>GUE</a>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🔒 Close", callback_data = "close")
+                        InlineKeyboardButton("Oke", callback_data = "close")
                     ]
                 ]
             )
